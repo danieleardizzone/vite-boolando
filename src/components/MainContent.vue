da370bb2b6e4e1112fce09d63683836541efd083
danieleardizzone <daniele.ardi02@gmail.com>
2023-11-06 19:33:24 +0100

    ADD - array products

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,10 +1,79 @@
-<script></script>
+<script>
+export default {
+    data() {
+        return {
+            products: [
+                {
+                    brandName: "Levi's",
+                    modelName: "RELAXED FIT TEE UNISEX",
+                    discountedPrice: "14,99",
+                    fullPrice: "29,99",
+                    discount: "-50%",
+                    sustainability: true,
+                    src: "1.webp",
+                    srcBis: "1b.webp",
+                },
+                {
+                    brandName: "Guess",
+                    modelName: "ROSES TEE",
+                    discountedPrice: "20,99",
+                    fullPrice: "29,99",
+                    discount: "-30%",
+                    sustainability: false,
+                    src: "2.webp",
+                    srcBis: "2b.webp",
+                },
+                {
+                    brandName: "Come Zucchero Filato",
+                    modelName: "VOGLIA DI COLORI PASTELLO",
+                    discountedPrice: "129,99",
+                    fullPrice: "184,99",
+                    discount: "-30%",
+                    sustainability: false,
+                    src: "3.webp",
+                    srcBis: "3b.webp",
+                },
+                {
+                    brandName: "Levi's",
+                    modelName: "TEE UNISEX",
+                    discountedPrice: "14,99",
+                    fullPrice: "29,99",
+                    discount: "-50%",
+                    sustainability: true,
+                    src: "4.webp",
+                    srcBis: "4b.webp",
+                },
+                {
+                    brandName: "Maya Deluxe",
+                    modelName: "STRIPE BODICE",
+                    discountedPrice: false,
+                    fullPrice: "99,99",
+                    discount: false,
+                    sustainability: false,
+                    src: "5.webp",
+                    srcBis: "5b.webp",
+                },
+                {
+                    brandName: "Esprit",
+                    modelName: "MAGLIONE - BLACK",
+                    discountedPrice: false,
+                    fullPrice: "14,99",
+                    discount: false,
+                    sustainability: true,
+                    src: "6.webp",
+                    srcBis: "6b.webp",
+                },
+            ]
+        }
+    }
+}
+</script>
 
 <template>
     <main class="main-content">
         <div class="container">
             <div class="row">
-                <div class="col col-1" v-for="n in 6" :key="n">
+                <div class="col col-1" v-for="(product, index) in products" :key="index">
                     <div class="card">
                         <figure class="card-image">
                             <img class="first" src="/img/1.webp">
@@ -12,15 +81,23 @@
                         </figure>
                         <div class="heart">&hearts;</div>
                         <ul class="mini-cards">
-                            <li class="red-card">-50%</li>
-                            <li class="green-card">Sostenibilità</li>
+                            <li class="red-card" :class="product.discount !== false ? '' : 'd-none'">
+                                {{ product.discount }}
+                            </li>
+                            <li class="green-card" :class="product.sustainability !== false ? '' : 'd-none'">
+                                Sostenibilità
+                            </li>
                         </ul>
                     </div>
-                    <p class="brand-name">Levi's</p>
-                    <p class="model-name">RELAXED FIT TEE UNISEX</p>
+                    <p class="brand-name">{{ product.brandName }}</p>
+                    <p class="model-name">{{ product.modelName }}</p>
                     <div class="price">
-                        <span class="actual-price">14,99&euro;</span>
-                        <span class="full-price">29,99&euro;</span>
+                        <span class="discounted-price" :class="product.discount !== false ? '' : 'd-none'">
+                            {{ product.discountedPrice }}&euro;
+                        </span>
+                        <span :class="product.discount !== false ? 'full-price' : 'discounted-price'">
+                            {{ product.fullPrice }}&euro;
+                        </span>
                     </div>
                 </div>
             </div>
@@ -39,7 +116,7 @@
     font-weight: 700;
 }
 
-.actual-price {
+.discounted-price {
     font-weight: 700;
     color: red;
 }
