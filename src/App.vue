41e90a332bfa33539484ee285ea6e3fc068dcbb0
danieleardizzone <daniele.ardi02@gmail.com>
2023-11-08 16:30:56 +0100

    ADD - json-server

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,32 @@
-<script setup>
+<script>
 import PageHeader from './components/PageHeader.vue';
 import MainContent from './components/MainContent.vue';
 import PageFooter from './components/PageFooter.vue';
+
+import axios from 'axios';
+
+import { store } from './store';
+
+export default {
+  components: {
+    PageHeader,
+    MainContent,
+    PageFooter
+  },
+  data() {
+    return {
+      productsUrl: 'http://localhost:3000/products',
+      store
+    }
+  }, mounted() {
+    axios.get(this.productsUrl)
+      .then(res => {
+        console.log(res.data);
+
+        this.store.products = res.data;
+      })
+  }
+}
 </script>
 
 <template>
